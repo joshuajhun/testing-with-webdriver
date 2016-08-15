90a2adf41134b6c48ebad0f1be5a098c16e93832
joshuajhun <joshuajhun@gmail.com>
2016-08-15 01:21:17 -0600

    added user stories to tests

---
+++ test/specs/client-test.js
@@ -0,0 +1,62 @@
+//warm up
+
+// As a user
+// when I visit the root of your application
+// there should be a title on your webpage
+
+
+// user story 1
+
+//As a user,
+// when I go to the root of your application.
+// I should see two input fields.
+// One for an idea title and one for an idea description.
+// When I fill out both fields and click the submit button .
+// I expect my idea to be posted onto the page.
+
+// user story 2
+
+// As a user,
+// When I submit a new idea
+// The input fields should clear
+
+// user story 3
+
+// As a user,
+// when I go to the root of your application.
+// I should see two input fields.
+// One for an idea title and one for an idea description.
+// When I fill out both fields and click the submit button .
+// I expect my idea to be posted onto the page.
+
+
+// user story 4
+
+//
+// As a user,
+// when I go to the root of your application
+// and I add a new idea to the page,
+// there should be a delete button attached to the idea.
+// When I click the delete idea button
+// I expect the idea to be taken off of the page.
+
+// user story 5
+
+// As a user,
+// When I go to the root of your application
+// and I submit and idea
+// the idea title and body should be saved into local storage.
+
+
+// user story 6
+
+// As a user
+// when i attempt to submit and idea with a title or description missing
+// the submit button should be inactive
+// but when I fill out both fields the button should become active.
+
+// user story 7
+
+// As a user
+// When I submit a new idea
+// my idea along with it's description should be saved to local storage.
